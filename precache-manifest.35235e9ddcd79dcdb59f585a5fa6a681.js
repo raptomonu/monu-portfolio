61468e643a6b778d04ec1677ffec276c01f8f3ad
Monu <raptomonu@gmail.com>
2023-02-01 23:38:27 +0530

    Updates

---
+++ precache-manifest.35235e9ddcd79dcdb59f585a5fa6a681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d754f792c61cb2610e",
+    "url": "/static/css/main.14b8aff8.chunk.css"
+  },
+  {
+    "revision": "99d754f792c61cb2610e",
+    "url": "/static/js/main.a050b8e2.chunk.js"
+  },
+  {
+    "revision": "8713b0f9eaf315f84d67",
+    "url": "/static/js/runtime~main.83ffa15f.js"
+  },
+  {
+    "revision": "2e85c84d62d6ce95317a",
+    "url": "/static/js/2.5c87726f.chunk.js"
+  },
+  {
+    "revision": "c3ab946e8c816a7dea2f",
+    "url": "/static/js/3.5890c759.chunk.js"
+  },
+  {
+    "revision": "bb6ee76037eadfd698afefc350d6140f",
+    "url": "/index.html"
+  }
+];
